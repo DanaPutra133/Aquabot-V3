61956b5dd4abb7aa819925189e924c257133f582
danaputra221 <danaputra221>
2024-02-06 19:14:53 +0700

    cekexpired api

---
+++ aqua bot/plugins/owner-cekexp.js
@@ -0,0 +1,17 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text }) => {
+if (!text) throw `Masukan Username Di Website Api!`
+  try {
+    let api = await fetch(`https://api.betabotz.eu.org/api/checkexp?username=${text}`)
+    let body = await api.text()
+    m.reply(body)  
+  } catch (e) {
+    console.log(e) 
+    m.reply('Username tidak terdaftar!')
+  }
+}          
+handler.command = handler.help = ['checkexp','cekexp', 'expapi'];
+handler.tags = ['main'];
+handler.rowner = true;
+
+module.exports = handler;
