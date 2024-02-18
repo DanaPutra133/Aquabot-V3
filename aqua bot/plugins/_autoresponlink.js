6ef4d05102f9ad133aa0b0c361395de65cdef57b
unknown <danaputra1001@gmail.com>
2024-02-18 16:01:56 +0700

    gantirespon

--- aqua bot/plugins/_autoresponlink.js
+++ aqua bot/plugins/_autoresponlink.js
@@ -5,7 +5,7 @@ handler.all = async function (m, { isBlocked }) {
     if (isBlocked) return
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
-    • permanent / Rp 8k 
+    pembuatan bot khusus untuk 1 penyewa 1 bot
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
