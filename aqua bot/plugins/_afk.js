3d7911dc67d8a92751d1c2a208c1a1562898ce8b
dana <danaputra1001@gmail.com>
2024-03-16 16:19:58 +0700

    fix afk update ui

--- aqua bot/plugins/_afk.js
+++ aqua bot/plugins/_afk.js
@@ -3,8 +3,8 @@ handler.before = m => {
   let user = global.db.data.users[m.sender]
   if (user.afk > -1) {
     m.reply(`
-Kamu berhenti AFK${user.afkReason ? ' setelah ' + user.afkReason : ''}
-Selama ${clockString(new Date - user.afk)}
+Kamu berhenti AFK 😀${user.afkReason ? ' \nsetelah: ' + user.afkReason : ''}
+Selama 🕰️: ${clockString(new Date - user.afk)}
 `.trim())
     user.afk = -1
     user.afkReason = ''
@@ -17,9 +17,9 @@ Selama ${clockString(new Date - user.afk)}
     if (!afkTime || afkTime < 0) continue
     let reason = user.afkReason || ''
     m.reply(`
-Jangan tag dia!
-Dia sedang AFK ${reason ? 'dengan alasan ' + reason : 'tanpa alasan'}
-Selama ${clockString(new Date - afkTime)}
+Jangan tag dia! ❌
+Dia sedang AFK 💤 ${reason ? 'dengan alasan: ' + reason : 'tanpa alasan'}
+Selama 🕰️ ${clockString(new Date - afkTime)}
 `.trim())
   }
   return true
