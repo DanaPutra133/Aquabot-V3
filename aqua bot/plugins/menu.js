44eba45666623ea2b94b4428c1dcf4a565cf9d20
dana <danaputra1001@gmail.com>
2024-10-26 00:09:53 +0700

    fix menu error

--- aqua bot/plugins/menu.js
+++ aqua bot/plugins/menu.js
@@ -1,13 +1,13 @@
 const { 
-  BufferJSON, 
-  WA_DEFAULT_EPHEMERAL, 
-  generateWAMessageFromContent, 
-  proto, 
-  generateWAMessageContent, 
-  generateWAMessage, 
-  prepareWAMessageMedia, 
-  areJidsSameUser, 
-  getContentType 
+    BufferJSON, 
+    WA_DEFAULT_EPHEMERAL, 
+    generateWAMessageFromContent, 
+    proto, 
+    generateWAMessageContent, 
+    generateWAMessage, 
+    prepareWAMessageMedia, 
+    areJidsSameUser, 
+    getContentType 
 } = require('@adiwajshing/baileys')
 
 process.env.TZ = 'Asia/Jakarta'
@@ -17,71 +17,71 @@ let fetch = require('node-fetch')
 let moment = require('moment-timezone')
 let levelling = require('../lib/levelling')
 
-let tags
+
 let arrayMenu = [
-  'main',
-  'ai',
-  'downloader',
-  'rpg',
-  'rpgG', 
-  'sticker',
-  'advanced',
-  'xp',
-  'fun',
-  'game',
-  'github',
-  'group',
-  'image',
-  'nsfw',
-  'info',
-  'internet',
-  'islam',
-  'kerang',
-  'maker',
-  'news',
-  'owner',
-  'voice',
-  'quotes',
-  'stalk', 
-  'store',
-  'shortlink',
-  'tools',
-  'anonymous',
+    'main',
+    'ai',
+    'downloader',
+    'rpg',
+    'sticker',
+    'rpgG', 
+    'advanced',
+    'xp',
+    'fun',
+    'game',
+    'github',
+    'group',
+    'image',
+    'nsfw',
+    'info',
+    'internet',
+    'islam',
+    'kerang',
+    'maker',
+    'news',
+    'owner',
+    'voice',
+    'quotes',
+    'stalk', 
+    'store',
+    'shortlink',
+    'tools',
+    'anonymous',
 ]
 
 const allTags = {
-  'main': 'MENU UTAMA',
-  'ai': 'MENU AI',
-  'downloader': 'MENU DOWNLOADER',
-  'rpg': 'MENU RPG',
-  'rpgG': 'MENU RPG GUILD',
-  'sticker': 'MENU CONVERT',
-  'advanced': 'ADVANCED',
-  'xp': 'MENU EXP',
-  'fun': 'MENU FUN',
-  'game': 'MENU GAME',
-  'github': 'MENU GITHUB',
-  'group': 'MENU GROUP',
-  'image': 'MENU IMAGE',
-  'nsfw': 'MENU NSFW',
-  'info': 'MENU INFO',
-  'internet': 'INTERNET',
-  'islam': 'MENU ISLAMI',
-  'kerang': 'MENU KERANG',
-  'maker': 'MENU MAKER',
-  'news': 'MENU NEWS',
-  'owner': 'MENU OWNER',
-  'voice': 'PENGUBAH SUARA',
-  'quotes': 'MENU QUOTES',
-  'stalk': 'MENU STALK',
-  'store': 'MENU STORE',
-  'shortlink': 'SHORT LINK',
-  'tools': 'MENU TOOLS',
-  'anonymous': 'ANONYMOUS CHAT'
+    'main': 'MENU UTAMA',
+    'ai': 'MENU AI',
+    'downloader': 'MENU DOWNLOADER',
+    'rpg': 'MENU RPG',
+    'rpgG': 'MENU RPG GUILD',
+    'sticker': 'MENU CONVERT',
+    'advanced': 'ADVANCED',
+    'xp': 'MENU EXP',
+    'fun': 'MENU FUN',
+    'game': 'MENU GAME',
+    'github': 'MENU GITHUB',
+    'group': 'MENU GROUP',
+    'image': 'MENU IMAGE',
+    'nsfw': 'MENU NSFW',
+    'info': 'MENU INFO',
+    'internet': 'INTERNET',
+    'islam': 'MENU ISLAMI',
+    'kerang': 'MENU KERANG',
+    'maker': 'MENU MAKER',
+    'news': 'MENU NEWS',
+    'owner': 'MENU OWNER',
+    'voice': 'PENGUBAH SUARA',
+    'quotes': 'MENU QUOTES',
+    'stalk': 'MENU STALK',
+    'store': 'MENU STORE',
+    'shortlink': 'SHORT LINK',
+    'tools': 'MENU TOOLS',
+    'anonymous': 'ANONYMOUS CHAT'
 }
 
 const defaultMenu = {
-  before: `
+    before: `
 Hi %name
 I am an automated system (WhatsApp Bot) that can help to do something, search and get data / information only through WhatsApp.
 
@@ -93,161 +93,147 @@ I am an automated system (WhatsApp Bot) that can help to do something, search an
 │  ◦ Waktu : %time
 └  ◦ Prefix Used : *[ %p ]*
 `.trimStart(),
-  header: '┌  ◦ *%category*',
-  body: '│  ◦ %cmd %islimit %isPremium',
-  footer: '└  ',
-  after: `*Note:* Ketik .menu <category> untuk melihat menu spesifik\nContoh: .menu tools`
+    header: '┌  ◦ *%category*',
+    body: '│  ◦ %cmd %islimit %isPremium',
+    footer: '└  ',
+    after: `*Note:* Ketik .menu <category> untuk melihat menu spesifik\nContoh: .menu tools`
 }
 
 let handler = async (m, { conn, usedPrefix: _p, args = [], command }) => {
-  try {
-      let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
-      let { exp, limit, level, role } = global.db.data.users[m.sender]
-      let { min, xp, max } = levelling.xpRange(level, global.multiplier)
-      let name = `@${m.sender.split`@`[0]}`
-      let teks = args[0] || ''
-      
-      // Time settings 
-      let d = new Date(new Date + 3600000)
-      let locale = 'id'
-      let date = d.toLocaleDateString(locale, {
-          day: 'numeric',
-          month: 'long',
-          year: 'numeric'
-      })
-      
-      let time = d.toLocaleTimeString(locale, {
-          hour: 'numeric',
-          minute: 'numeric',
-          second: 'numeric'
-      })
+    try {
+        let package = JSON.parse(await fs.promises.readFile(path.join(__dirname, '../package.json')).catch(_ => '{}'))
+        let { exp, limit, level, role } = global.db.data.users[m.sender]
+        let { min, xp, max } = levelling.xpRange(level, global.multiplier)
+        let name = `@${m.sender.split`@`[0]}`
+        let teks = args[0] || ''
+        
+        // Time settings 
+        let d = new Date(new Date + 3600000)
+        let locale = 'id'
+        let date = d.toLocaleDateString(locale, {
+            day: 'numeric',
+            month: 'long',
+            year: 'numeric'
+        })
+        
+        let time = d.toLocaleTimeString(locale, {
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric'
+        })
 
-      let _uptime = process.uptime() * 1000
-      let uptime = clockString(_uptime)
-      
-      // Get help list
-      let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
-          return {
-              help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
-              tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
-              prefix: 'customPrefix' in plugin,
-              limit: plugin.limit,
-              premium: plugin.premium,
-              enabled: !plugin.disabled,
-          }
-      })
+        let _uptime = process.uptime() * 1000
+        let uptime = clockString(_uptime)
+        
+        // Get help list
+        let help = Object.values(global.plugins).filter(plugin => !plugin.disabled).map(plugin => {
+            return {
+                help: Array.isArray(plugin.tags) ? plugin.help : [plugin.help],
+                tags: Array.isArray(plugin.tags) ? plugin.tags : [plugin.tags],
+                prefix: 'customPrefix' in plugin,
+                limit: plugin.limit,
+                premium: plugin.premium,
+                enabled: !plugin.disabled,
+            }
+        })
 
-      // main menu 
-      if (!teks) {
-          let menuList = `${defaultMenu.before}\n\n┌  ◦ *DAFTAR MENU*\n`
-          for (let tag of arrayMenu) {
-              if (tag && allTags[tag]) {
-                  menuList += `│  ◦ ${_p}menu ${tag}\n`
-              }
-          }
-          menuList += `└  \n\n${defaultMenu.after}`
+        // main menu 
+        if (!teks) {
+            let menuList = `${defaultMenu.before}\n\n┌  ◦ *DAFTAR MENU*\n`
+            for (let tag of arrayMenu) {
+                if (tag && allTags[tag]) {
+                    menuList += `│  ◦ ${_p}menu ${tag}\n`
+                }
+            }
+            menuList += `└  \n\n${defaultMenu.after}`
 
-          // Replace placeholders nya
-          let replace = {
-              '%': '%',
-              p: _p, 
-              uptime,
-              name, 
-              date,
-              time
-          }
+            // Replace placeholders nya
+            let replace = {
+                '%': '%',
+                p: _p, 
+                uptime,
+                name, 
+                date,
+                time
+            }
 
-          menuList = menuList.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
-              (_, name) => '' + replace[name])
+            let text = menuList.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
+                (_, name) => '' + replace[name])
 
-          await conn.relayMessage(m.chat, {
-              extendedTextMessage:{
-                  text: menuList,
-                  contextInfo: {
-                      mentionedJid: [m.sender],
-                      externalAdReply: {
-                          title: date,
-                          mediaType: 1,
-                          previewType: 0,
-                          renderLargerThumbnail: true,
-                          thumbnailUrl: 'https://telegra.ph/file/3a34bfa58714bdef500d9.jpg',
-                          sourceUrl: 'https://whatsapp.com/channel/0029Va8ZH8fFXUuc69TGVw1q'
-                      }
-                  },
-                  mentions: [m.sender]
-              }
-          }, {})
-          return
-      }
+            await conn.relayMessage(m.chat, {
+                extendedTextMessage:{
+                    text: text,
+                    contextInfo: {
+                        mentionedJid: [m.sender],
+                        externalAdReply: {
+                            title: date,
+                            mediaType: 1,
+                            previewType: 0,
+                            renderLargerThumbnail: true,
+                            thumbnailUrl: 'https://telegra.ph/file/3a34bfa58714bdef500d9.jpg',
+                            sourceUrl: 'https://whatsapp.com/channel/0029Va8ZH8fFXUuc69TGVw1q'
+                        }
+                    },
+                    mentions: [m.sender]
+                }
+            }, {})
+            return
+        }
 
-      if (!allTags[teks]) {
-          return m.reply(`Menu "${teks}" tidak tersedia.\nSilakan ketik ${_p}menu untuk melihat daftar menu.`)
-      }
+        if (!allTags[teks]) {
+            return m.reply(`Menu "${teks}" tidak tersedia.\nSilakan ketik ${_p}menu untuk melihat daftar menu.`)
+        }
 
-      tags = { [teks]: allTags[teks] }
-      
-      let groups = {}
-      for (let tag in tags) {
-          groups[tag] = []
-          for (let plugin of help)
-              if (plugin.tags && plugin.tags.includes(tag))
-                  if (plugin.help) groups[tag].push(plugin)
-      }
+        let menuCategory = defaultMenu.before + '\n\n'
+        menuCategory += defaultMenu.header.replace(/%category/g, allTags[teks]) + '\n'
+        
+        let categoryCommands = help.filter(menu => menu.tags && menu.tags.includes(teks) && menu.help)
+        
+        for (let menu of categoryCommands) {
+            for (let help of menu.help) {
+                menuCategory += defaultMenu.body
+                    .replace(/%cmd/g, menu.prefix ? help : _p + help)
+                    .replace(/%islimit/g, menu.limit ? '(Ⓛ)' : '')
+                    .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '') + '\n'
+            }
+        }
+        
+        menuCategory += defaultMenu.footer + '\n\n' + defaultMenu.after
 
-      let _text = [
-          defaultMenu.before,
-          ...Object.keys(tags).map(tag => {
-              return defaultMenu.header.replace(/%category/g, tags[tag]) + '\n' + [
-                  ...help.filter(menu => menu.tags && menu.tags.includes(tag) && menu.help).map(menu => {
-                      return menu.help.map(help => {
-                          return defaultMenu.body.replace(/%cmd/g, menu.prefix ? help : '%p' + help)
-                              .replace(/%islimit/g, menu.limit ? '(Ⓛ)' : '')
-                              .replace(/%isPremium/g, menu.premium ? '(Ⓟ)' : '')
-                              .trim()
-                      }).join('\n')
-                  }),
-                  defaultMenu.footer
-              ].join('\n')
-          }),
-          defaultMenu.after
-      ].join('\n')
+        let replace = {
+            '%': '%',
+            p: _p, 
+            uptime, 
+            name,
+            date,
+            time
+        }
 
-      let text = typeof conn.menu == 'string' ? conn.menu : typeof conn.menu == 'object' ? _text : ''
-      
-      let replace = {
-          '%': '%',
-          p: _p, 
-          uptime, 
-          name,
-          date,
-          time
-      }
+        let text = menuCategory.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
+            (_, name) => '' + replace[name])
 
-      text = text.replace(new RegExp(`%(${Object.keys(replace).sort((a, b) => b.length - a.length).join`|`})`, 'g'), 
-          (_, name) => '' + replace[name])
-      
-      await conn.relayMessage(m.chat, {
-          extendedTextMessage:{
-              text: text, 
-              contextInfo: {
-                  mentionedJid: [m.sender],
-                  externalAdReply: {
-                      title: date,
-                      mediaType: 1,
-                      previewType: 0,
-                      renderLargerThumbnail: true,
-                      thumbnailUrl: 'https://telegra.ph/file/3a34bfa58714bdef500d9.jpg',
-                      sourceUrl: 'https://whatsapp.com/channel/0029VaApYsQ5Ui2c2rKbpP0S'
-                  }
-              }, 
-              mentions: [m.sender]
-          }
-      }, {})
+        await conn.relayMessage(m.chat, {
+            extendedTextMessage:{
+                text: text, 
+                contextInfo: {
+                    mentionedJid: [m.sender],
+                    externalAdReply: {
+                        title: date,
+                        mediaType: 1,
+                        previewType: 0,
+                        renderLargerThumbnail: true,
+                        thumbnailUrl: 'https://telegra.ph/file/3a34bfa58714bdef500d9.jpg',
+                        sourceUrl: 'https://whatsapp.com/channel/0029Va8ZH8fFXUuc69TGVw1q'
+                    }
+                }, 
+                mentions: [m.sender]
+            }
+        }, {})
 
-  } catch (e) {
-      conn.reply(m.chat, 'Maaf, menu sedang error', m)
-      console.error(e)
-  }
+    } catch (e) {
+        conn.reply(m.chat, 'Maaf, menu sedang error', m)
+        console.error(e)
+    }
 }
 
 handler.help = ['menu']
@@ -258,9 +244,9 @@ handler.exp = 3
 module.exports = handler
 
 function clockString(ms) {
-  if (isNaN(ms)) return '--'
-  let h = Math.floor(ms / 3600000)
-  let m = Math.floor(ms / 60000) % 60
-  let s = Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+    if (isNaN(ms)) return '--'
+    let h = Math.floor(ms / 3600000)
+    let m = Math.floor(ms / 60000) % 60
+    let s = Math.floor(ms / 1000) % 60
+    return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 }
