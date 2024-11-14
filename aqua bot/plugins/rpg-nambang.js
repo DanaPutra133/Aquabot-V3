e4e23c23df6456101d2d3a8d8b5dea29ed728487
dana <danaputra1001@gmail.com>
2024-11-14 16:55:37 +0700

    nambang v2

--- aqua bot/plugins/rpg-nambang.js
+++ aqua bot/plugins/rpg-nambang.js
@@ -1,50 +1,75 @@
-const timeout = 28800000
+let handler = async (m, { conn }) => {
+  let __timers = (new Date - global.db.data.users[m.sender].lastnambang)
+  let _timers = (300000 - __timers)
+  let timers = clockString(_timers) 
+let name = conn.getName(m.sender)
+  let user = global.db.data.users[m.sender]
+  
+   if (new Date - global.db.data.users[m.sender].lastnambang > 300000) {
+let randomaku1 = `${Math.floor(Math.random() * 10)}`
+let randomaku2 = `${Math.floor(Math.random() * 10)}`
+let randomaku4 = `${Math.floor(Math.random() * 5)}`
+let randomaku3 = `${Math.floor(Math.random() * 10)}`
+let randomaku5 = `${Math.floor(Math.random() * 10)}`
 
-let handler = async (m, { conn, usedPrefix, text }) => {
-  //      let timerand = `${Math.floor(Math.random() * 259200000)}`.trim()
-	    let time = global.db.data.users[m.sender].lastnambang + 28800000
-  if (new Date - global.db.data.users[m.sender].lastnambang< 28800000) throw `Anda sudah menambang\nMohon tunggu hasil pertambangan mu\nTunggu selama ${msToTime(time - new Date())} lagi`
-   // if (global.db.data.users[m.sender].aqua > 9) {
-   // let aquah = `${Math.floor(Math.random() * 5)}`.trim()
-    let berlians = `${Math.floor(Math.random() * 3)}`.trim()
-    let emasbiasas = `${Math.floor(Math.random() * 4)}`.trim()
-    let emasbatangs = `${Math.floor(Math.random() * 3)}`.trim()
-    // global.db.data.users[m.sender].aqua -= aquah * 1
-    global.db.data.users[m.sender].berlian += berlians * 1
-    global.db.data.users[m.sender].emas += emasbiasas * 1
-    global.db.data.users[m.sender].diamond += emasbatangs * 1
-    global.db.data.users[m.sender].tiketcoin += 1
-	/*let pisangnye = `${Math.floor(Math.random(global.db.data.users[m.sender].pisang = pisangpoin) * 1)}`.trim()
-	let jeruknye = `${Math.floor(Math.random(global.db.data.users[m.sender].jeruk = jerukpoin) * 1)}`.trim()
-	let mangganye = `${Math.floor(Math.random(global.db.data.users[m.sender].mangga = manggapoin) * 1)}`.trim()
-	let anggurnye = `${Math.floor(Math.random(global.db.data.users[m.sender].anggur = anggurpoin) * 1)}`.trim()
-	let apelnye = `${Math.floor(Math.random(global.db.data.users[m.sender].apel = apelpoin) * 10000)}`.trim()*/
-	global.db.data.users[m.sender].lastnambang = new Date * 1
- // else conn.reply(m.chat, 'Minimal 10 Aqua untuk kamu menambang, karena menambang membutuhkan energi, seperti mendapat dia >,,<', m)
-  conn.reply(m.chat, `Selamat kamu mendapatkan : \n+${berlians} Berlian\n+${emasbiasas} Emas\n+${emasbatangs} Diamond\n\n+1 Tiketcoin`, m)
+.trim()
+
+let rbrb1 = (randomaku1 * 2)
+let rbrb2 = (randomaku2 * 1) 
+let rbrb3 = (randomaku3 * 1)
+let rbrb4 = (randomaku4 * 15768)
+let rbrb5 = (randomaku5 * 1)
+
+var zero1 = `${rbrb1}`
+var zero2 = `${rbrb2}`
+var zero3 = `${rbrb3}`
+var zero4 = `${rbrb4}`
+var zero5 = `${rbrb5}`
+
+let arr = [
+`mencari lebih dalam...`, 
+`⛏️⛏️🪨💎🪨🪨🪨🪨🪨
+🪨⬜⬜⬜🪨⬜⬜⬜🪨🪨
+🪨🪨🪨🪨🪨🪨🪨🪨🪨🪨
+🪨🪨🪨⛏️⛏️🪙  🪙 🪨       \n\n\n➕ mulai menambang....`, 
+`🪨🪨🪨🪨🪨🪨🪨🪨🪨
+💎⛏️⛏️🪨🪨⬜⬜⬜🪨🪨
+🪨🪨🪨🪨🪨🪨🪨⛏️🪨🪨
+🪨🪨⛏️⛏️🪙  🪙 🪨       \n\n\n➕ kamu di tambang...`, 
+`➕ 💹Mendapatkan hasil tambang....`, 
+`*—[ Hasil nambang kamu ${name} ]—*
+➕ 💹 berlian = [ ${zero3} ]
+➕ ✨ emas = [ ${zero4} ] 	
+➕ ✨ diamond = [ ${zero5} ] 		 
+${wm}`
+]
+
+let { key } = await conn.sendMessage(m.chat, {text: 'mencari tempat nambang.....'})
+for (let i = 0; i < arr.length; i++) {
+await new Promise(resolve => setTimeout(resolve, 10000));
+await conn.sendMessage(m.chat, { text: arr[i], edit: key });
+}
+
+global.db.data.users[m.sender].berlian += rbrb5
+global.db.data.users[m.sender].emas += rbrb4 
+global.db.data.users[m.sender].diamond += rbrb3
+global.db.data.users[m.sender].tiketcoin += 1
+
+user.lastnambang = new Date * 1
+  } else m.reply(`Sepertinya anda sudah kecapekan dari tambang... silahkan istirahat dulu sekitar\n*${timers}*`)
 }
 handler.help = ['nambang']
 handler.tags = ['rpg']
-handler.command = /^(nambang)/i
-handler.group = true
-
-handler.fail = null
-handler.limit = true
-handler.exp = 0
-handler.money = 0
+handler.command = /^(nambang)$/i
+handler.register = true
 
 module.exports = handler
 
-function msToTime(duration) {
-  var milliseconds = parseInt((duration % 1000) / 100),
-    seconds = Math.floor((duration / 1000) % 60),
-    minutes = Math.floor((duration / (1000 * 60)) % 60),
-    hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
-    
-  
-  hours = (hours < 10) ? "0" + hours : hours
-  minutes = (minutes < 10) ? "0" + minutes : minutes
-  seconds = (seconds < 10) ? "0" + seconds : seconds
 
-  return hours + " jam " + minutes + " menit " + seconds + " detik"
+function clockString(ms) {
+let h = Math.floor(ms / 3600000)
+let m = Math.floor(ms / 60000) % 60
+let s = Math.floor(ms / 1000) % 60
+console.log({ms,h,m,s})
+return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
 }
