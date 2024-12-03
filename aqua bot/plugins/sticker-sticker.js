abb7f2db1a76ac46e89cd020b68dece283d5a080
dana <danaputra1001@gmail.com>
2024-12-03 21:34:21 +0700

    fix sticker

--- aqua bot/plugins/sticker-sticker.js
+++ aqua bot/plugins/sticker-sticker.js
@@ -1,26 +1,36 @@
 const fs = require('fs')
+const sharp = require('sharp') 
 
 let handler = async (m, { conn, command, usedPrefix }) => {
-let q = m.quoted ? m.quoted : m
-let mime = (q.msg || q).mimetype || ''
-if (/image/.test(mime)) {
-  let media = await q.download()
-  let encmedia = await conn.sendImageAsSticker(m.chat, media, m, { packname: global.packname, author: global.author })
-  await fs.unlinkSync(encmedia)
+  let q = m.quoted ? m.quoted : m
+  let mime = (q.msg || q).mimetype || ''
+  if (/image/.test(mime)) {
+    let media = await q.download()
+
+
+    let processedMedia = await sharp(media)
+      .resize(512, 512, { 
+        fit: 'contain',
+        background: { r: 255, g: 255, b: 255, alpha: 0 } 
+      })
+      .png() 
+      .toBuffer()
+
+    let encmedia = await conn.sendImageAsSticker(m.chat, processedMedia, m, { packname: global.packname, author: global.author })
+    await fs.unlinkSync(encmedia)
   } else if (/video/.test(mime)) {
-  if ((q.msg || q).seconds > 7) return m.reply('maksimal 6 detik!')
-  let media = await q.download()
-  let encmedia = await conn.sendVideoAsSticker(m.chat, media, m, { packname: global.packname, author: global.author })
-  await fs.unlinkSync(encmedia)
+    if ((q.msg || q).seconds > 7) return m.reply('Maksimal 6 detik!')
+    let media = await q.download()
+
+    let encmedia = await conn.sendVideoAsSticker(m.chat, media, m, { packname: global.packname, author: global.author })
+    await fs.unlinkSync(encmedia)
   } else {
-  throw `Kirim Gambar/Video Dengan Caption ${usedPrefix + command}\nDurasi Video 1-6 Detik`
+    throw `Kirim gambar/video dengan caption ${usedPrefix + command}\nDurasi video 1-6 detik.`
   }
-    }
+}
+
 handler.help = ['sticker']
 handler.tags = ['sticker']
 handler.command = /^(stiker|s|sticker)$/i
+handler.limit = true
 module.exports = handler
-
-const isUrl = (text) => {
-    return text.match(new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&/=]*)(jpe?g|gif|png|mp4)/, 'gi'))
-}
