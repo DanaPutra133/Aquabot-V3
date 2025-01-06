a069106334e9cd2a519681796e780913d34972fb
dana <danaputra1001@gmail.com>
2025-01-06 10:00:47 +0700

    fix demote and promote grub

--- aqua bot/plugins/group-demote.js
+++ aqua bot/plugins/group-demote.js
@@ -1,31 +1,44 @@
-let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
-	if (!(isAdmin || isOwner)) {
-                global.dfail('admin', m, conn)
-                throw false
-                }
+let handler = async (m, { teks, conn, isOwner, isAdmin, args, command }) => {
+  if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn);
+      throw false;
+  }
+
   let ownerGroup = m.chat.split`-`[0] + "@s.whatsapp.net";
-  if(m.quoted){
-if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
-let usr = m.quoted.sender;
-let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "demote"); return;
-if (nenen) m.reply(`sukses demote @${user.split('@')[0]}!`)
-}
-  if (!m.mentionedJid[0]) throw `tag yang mau diturunkan jabatannya`;
+
+  if (m.quoted) {
+      if (m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
+      let usr = m.quoted.sender;
+      await conn.groupParticipantsUpdate(m.chat, [usr], "demote");
+      m.reply(`Sukses demote @${usr.split('@')[0]}!`, null, { mentions: [usr] });
+      return;
+  }
+
+  if (!m.mentionedJid[0]) throw `Tag siapa yang ingin diturunkan jabatannya?`;
+
   let users = m.mentionedJid.filter(
-    (u) => !(u == ownerGroup || u.includes(conn.user.jid))
+      (u) => !(u == ownerGroup || u.includes(conn.user.jid))
   );
-  for (let user of users)
-    if (user.endsWith("@s.whatsapp.net"))
-      await conn.groupParticipantsUpdate(m.chat, [user], "demote");
+
+  for (let user of users) {
+      if (user.endsWith("@s.whatsapp.net")) {
+          await conn.groupParticipantsUpdate(m.chat, [user], "demote");
+          m.reply(`Sukses ${command} @${user.split('@')[0]}!`, null, { mentions: [user] });
+      }
+  }
 };
 
-handler.help = ['demote @user']
-handler.tags = ['group', 'owner']
-handler.command = /^(demo?te|member|\↓)$/i
+handler.help = ['demote @user'];
+handler.tags = ['group', 'owner'];
+handler.command = /^(demo?te|\↓)$/i;
 
-handler.group = true
-handler.botAdmin = true
-handler.admin = true
-handler.fail = null
+handler.group = true;
+handler.botAdmin = true;
+handler.admin = true;
+handler.fail = null;
 
-module.exports = handler
+module.exports = handler;
+
+
+//fix bagian ketika comamnd berjalan mreplay tidak mau terkirim
+//m.reply('Sukses demote @${usr.split('@')[0]}!',  null, {
--- aqua bot/plugins/group-promote.js
+++ aqua bot/plugins/group-promote.js
@@ -1,31 +1,43 @@
-let handler = async (m, { teks, conn, isOwner, isAdmin, args }) => {
-	if (!(isAdmin || isOwner)) {
-                global.dfail('admin', m, conn)
-                throw false
-                }
+let handler = async (m, { teks, conn, isOwner, isAdmin, args, command }) => {
+  if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn);
+      throw false;
+  }
+
   let ownerGroup = m.chat.split`-`[0] + "@s.whatsapp.net";
-  if(m.quoted){
-if(m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
-let usr = m.quoted.sender;
-let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "promote"); return;
-if (nenen) m.reply(`sukses promote @${user.split('@')[0]}!`);
-}
-  if (!m.mentionedJid[0]) throw `tag yang mau dinaikan jabatannya`;
+
+  if (m.quoted) {
+      if (m.quoted.sender === ownerGroup || m.quoted.sender === conn.user.jid) return;
+      let usr = m.quoted.sender;
+      let nenen = await conn.groupParticipantsUpdate(m.chat, [usr], "promote"); 
+      if (nenen) m.reply(`Sukses menaikan jabatan @${usr.split('@')[0]}!`, null, { mentions: [usr] });
+      return;
+  }
+
+  if (!m.mentionedJid[0]) throw `Tag siapa yang ingin dinaikkan jabatannya?`;
+
   let users = m.mentionedJid.filter(
-    (u) => !(u == ownerGroup || u.includes(conn.user.jid))
+      (u) => !(u == ownerGroup || u.includes(conn.user.jid))
   );
-  for (let user of users)
-    if (user.endsWith("@s.whatsapp.net"))
-      await conn.groupParticipantsUpdate(m.chat, [user], "promote");
+
+  for (let user of users) {
+      if (user.endsWith("@s.whatsapp.net")) {
+          await conn.groupParticipantsUpdate(m.chat, [user], "promote");
+          m.reply(`Sukses ${command} @${user.split('@')[0]}!`, null, { mentions: [user] });
+      }
+  }
 };
 
-handler.help = ['promote @user']
-handler.tags = ['group', 'owner']
-handler.command = /^(promo?te|admin|\^)$/i
+handler.help = ['promote @user'];
+handler.tags = ['group', 'owner'];
+handler.command = /^(promo?te|admin|\^)$/i;
 
-handler.group = true
-handler.botAdmin = true
-handler.admin = true
-handler.fail = null
+handler.group = true;
+handler.botAdmin = true;
+handler.admin = true;
+handler.fail = null;
 
-module.exports = handler
+module.exports = handler;
+
+//fix bagian ketika comamnd berjalan mreplay tidak mau terkirim
+//m.reply('Sukses promote @${usr.split('@')[0]}!', null, {
