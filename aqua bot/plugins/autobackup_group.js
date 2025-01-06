54819d80cd87760d4149731a82724a32d812e823
dana <danaputra1001@gmail.com>
2025-01-06 17:20:09 +0700

    change module to interval and add send to owner

--- aqua bot/plugins/autobackup_group.js
+++ aqua bot/plugins/autobackup_group.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const schedule = require('node-schedule');
-
 
 const sendDatabaseToGroup = async (conn, groupJid) => {
     try {
@@ -25,23 +23,30 @@ const sendDatabaseToGroup = async (conn, groupJid) => {
         console.error('Error saat mengirim file database:', err);
     }
 };
-const scheduleSendDatabase = (conn, groupJid) => {
-    schedule.scheduleJob('00 22 * * *', async () => {   
-        // 00 22 * * * artinya backup setiap jam 22:00
-        // 00 -> menit 22 -> jam
-        console.log('Memulai pengiriman database pada jam 22:00...');
-        try {
-            await sendDatabaseToGroup(conn, groupJid); 
-        } catch (err) {
-            console.error('Terjadi kesalahan saat mengirim:', err);
+
+const scheduleSendDatabaseWithInterval = (conn, groupJid) => {
+    const intervalTime = 60 * 1000; // jangan di hapus ini buat cek interval
+
+    setInterval(async () => {
+        const now = new Date();
+        if (now.getHours() === 5 && now.getMinutes() === 0) {
+            console.log('Memulai pengiriman database pada jam 5:00...');
+            // ganti angka 5 nya sama jam yang kalian mau, 9 = 9 pagi, 20 = 8 malam
+            try {
+                await sendDatabaseToGroup(conn, groupJid);
+            } catch (err) {
+                console.error('Terjadi kesalahan saat mengirim:', err);
+            }
         }
-    });
+    }, intervalTime);
+
+    console.log('interval untuk pengiriman db ke group di mulai!.');
 };
 
-const groupJid = '120363216901617825@g.us';
+const groupJid = '120363216901617825@g.us'; //isi dengan id grub
 
 if (global.conn) {
-    scheduleSendDatabase(global.conn, groupJid);
+    scheduleSendDatabaseWithInterval(global.conn, groupJid);
 } else {
     console.error('Koneksi ke grup belum ada!');
 }
